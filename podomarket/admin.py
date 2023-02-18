a063a81c6747779fe823d890463a909e7a25b618
znak99 <xhahekcldhkznak0113@gmail.com>
2023-02-18 23:30:27 +0900

    Post model modeling

--- podomarket/admin.py
+++ podomarket/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Post
 
 admin.site.register(User, UserAdmin)
 UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "line_id", "address")}), )
+
+admin.site.register(Post)
--- podomarket/models.py
+++ podomarket/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from .validators import validate_no_special_characters
 
 class User(AbstractUser):
@@ -21,4 +22,25 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.email
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    item_price = models.IntegerField(validators=[MinValueValidator(1)])
+    CONDITION_CHOICES = [
+        ('새제품', '新品'),
+        ('최상', '美品'),
+        ('상', '上'),
+        ('중', '中'),
+        ('하', '下'),
+    ]
+    item_condition = models.CharField(max_length=10, choices=CONDITION_CHOICES)
+    item_details = models.TextField(blank=True)
+    image1 = models.ImageField(upload_to='item_pics')
+    image2 = models.ImageField(upload_to='item_pics', blank = True)
+    image3 = models.ImageField(upload_to='item_pics', blank = True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.title
