77982be140e4ec08daf378dfe30819a4263b5ad6
znak99 <xhahekcldhkznak0113@gmail.com>
2023-02-22 16:58:32 +0900

    Create main-page and input test data

--- podomarket/views.py
+++ podomarket/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic import ListView
 from allauth.account.views import PasswordChangeView
+from podomarket.models import Post
 
-def index(request):
-    return render(request, 'podomarket/index.html')
+class IndexView(ListView):
+    model = Post
+    template_name = "podomarket/index.html"
+    context_object_name = "posts"
+    paginate_by = 8
+    ordering = ["-dt_updated"]
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
