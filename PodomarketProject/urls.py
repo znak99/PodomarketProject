f2097cd7424663de15c44a33018fd9b76996de52
znak99 <xhahekcldhkznak0113@gmail.com>
2023-02-26 02:32:58 +0900

    Access control detail, create

--- PodomarketProject/urls.py
+++ PodomarketProject/urls.py
@@ -9,8 +9,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('podomarket.urls')),
     path(
+        'email-confirmation-required/',
+        TemplateView.as_view(template_name='account/email_confirmation_required.html'),
+        name='account_email_confirmation_required',
+    ),
+    path(
         'email-confirmation-done/',
-        TemplateView.as_view(template_name='podomarket/email_confirmation_done.html'),
+        TemplateView.as_view(template_name='account/email_confirmation_done.html'),
         name='account_email_confirmation_done',
     ),
     path('password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
--- podomarket/views.py
+++ podomarket/views.py
@@ -7,9 +7,12 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+from braces.views import LoginRequiredMixin, UserPassesTestMixin
+from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordChangeView
 from podomarket.models import Post
 from .forms import PostCreateForm, PostUpdateForm
+from .functions import confirmation_required_redirect
 
 class IndexView(ListView):
     model = Post
@@ -18,15 +21,17 @@ class IndexView(ListView):
     paginate_by = 8
     ordering = ["-dt_updated"]
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "podomarket/post_detail.html"
     pk_url_kwarg = "post_id"
     
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     form_class = PostCreateForm
     template_name = 'podomarket/post_form.html'
+    redirect_unauthenticated_users = True
+    raise_exception = confirmation_required_redirect
     
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id': self.object.id})
@@ -34,6 +39,9 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self, user):
+        return EmailAddress.objects.filter(user=user, verified=True).exists()
     
 class PostUpdateView(UpdateView):
     model = Post
