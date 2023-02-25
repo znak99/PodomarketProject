2da70754d48f2f491a9717fef226c722f1df7d2a
znak99 <xhahekcldhkznak0113@gmail.com>
2023-02-26 02:46:13 +0900

    Access control update, delete

--- podomarket/views.py
+++ podomarket/views.py
@@ -43,22 +43,36 @@ class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     def test_func(self, user):
         return EmailAddress.objects.filter(user=user, verified=True).exists()
     
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostUpdateForm
     template_name = "podomarket/post_form.html"
     pk_url_kwarg = "post_id"
     
+    raise_exception = True
+    redirect_unauthenticated_users = False
+    
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id': self.object.id})
+    
+    def test_func(self, user):
+        post = self.get_object()
+        return post.author == user
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "podomarket/post_confirm_delete.html"
     pk_url_kwarg = "post_id"
     
+    raise_exception = True
+    redirect_unauthenticated_users = False
+    
     def get_success_url(self):
         return reverse('index')
+    
+    def test_func(self, user):
+        post = self.get_object()
+        return post.author == user
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
