63ee14bece881c524bc14e5473686406cba5e779
znak99 <xhahekcldhkznak0113@gmail.com>
2023-03-06 19:39:45 +0900

    Set ProfileSetupMiddleware

--- PodomarketProject/settings.py
+++ PodomarketProject/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'podomarket.middleware.ProfileSetupMiddleware',
 ]
 
 ROOT_URLCONF = 'PodomarketProject.urls'
